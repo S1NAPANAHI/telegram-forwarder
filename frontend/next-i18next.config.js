bf4d1aa42248ba99bd1f749d536df3b802ee5762
SINA PANAHI <93608711+S1NAPANAHI@users.noreply.github.com>
2025-10-14 23:53:06 +0200

    Set default language to Farsi (fa) for next-i18next config

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -1,9 +1,7 @@
-const path = require('path');
-
-module.exports = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'fa'], // Add your supported locales here
-  },
-  localePath: path.resolve('./public/locales'),
-};
+{
+  "locales": ["fa", "en"],
+  "defaultLocale": "fa",
+  "pages": {
+    "*": ["common"]
+  }
+}
