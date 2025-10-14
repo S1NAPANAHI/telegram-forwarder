451d3e99cb6c86466c67ce0bbf9a634cea4d6d9f
SINA PANAHI <93608711+S1NAPANAHI@users.noreply.github.com>
2025-10-15 00:19:11 +0200

    Update next.config.js to import and reuse i18n config from next-i18next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
+const i18nextConfig = require('./next-i18next.config');
+
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['fa', 'en'],
-    defaultLocale: 'fa',
-  },
+  i18n: i18nextConfig.i18n
 };
 
 module.exports = nextConfig;
