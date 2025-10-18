125f111f61335b82fc32aadd0cb95ed0973631d6
S1NAPANAHI <sina.panahi200@gmail.com>
2025-10-18 13:02:29 +0200

    fix: correct api import path in telegram-client page

--- frontend/pages/telegram-client.tsx
+++ frontend/pages/telegram-client.tsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import Layout from '../components/Layout';
 import { withAuth } from '../lib/withAuth';
-import api from '../../lib/api';
+import api from '../lib/api';
 import { useState } from 'react';
 
 const TelegramClientPage = () => {
